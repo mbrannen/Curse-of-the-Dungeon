0fdc45470c0f5e4d3e37175ec8df01c14f35a9ea
charles <charles.98006@gmail.com>
2024-01-18 20:08:06 -0800

    WIP

---
+++ TriggerManager.cs
@@ -0,0 +1,39 @@
+﻿using GameJam2024.RuneTree.FireRunes;
+using GameJam2024.RuneTree.IceRunes;
+using GameJam2024.RuneTree.LightningRunes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameJam2024;
+
+/// <summary>
+/// Class <c>TriggerManager</c> handles all of the events relating to the trigger interactable items and environments.
+/// </summary>
+public sealed class TriggerManager
+{
+    private TriggerManager(){}
+    private void EventTriggerHandler(EventObject event)
+    {
+        int day = 4;
+        switch (action) 
+        {
+            case "BURN":
+                // code block
+                break;
+            case "BLAST":
+                // code block
+                break;
+            case "BLAST":
+                // code block
+                break;
+            case "":
+                // code block
+                break;
+            default:
+                break;
+        }
+    }
+}
