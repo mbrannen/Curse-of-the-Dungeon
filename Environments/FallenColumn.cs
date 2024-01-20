f813bd312b1ebd9bbf83ff209231405975c2af96
charles <charles.98006@gmail.com>
2024-01-19 18:39:06 -0800

    Getting all of my changes back

---
+++ Environments/FallenColumn.cs
@@ -0,0 +1,24 @@
+﻿using GameJam2024.RuneTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameJam2024.Environments
+{
+    public class FallenColumn : IEnvironmentObject
+    {
+        private EnvironmentObjectStatus status;
+        private EnvironmentObjectType objectType;
+
+        public override EnvironmentObjectStatus envObjectStatus { get; } = EnvironmentObjectStatus.Normal;
+
+        public EnvironmentObjectStatus EnvironmentObjectStatus => throw new NotImplementedException();
+
+        public EnvironmentObjectType EnvironmentObjectType => throw new NotImplementedException();
+
+        public bool envObjectType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        bool IEnvironmentObject.envObjectStatus { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ Environments/IEnvironmentObject.cs
@@ -0,0 +1,14 @@
+﻿using GameJam2024.Environments;
+using GameJam2024.RuneTree;
+
+namespace GameJam2024
+{
+    public interface IEnvironmentObject
+    {
+        public EnvironmentObjectStatus EnvironmentObjectStatus { get; }
+        public EnvironmentObjectType EnvironmentObjectType { get; }
+        public bool envObjectStatus { get; set; }
+        public bool envObjectType { get; set; }
+
+    }
+}
