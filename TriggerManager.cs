ce6c4a8208e3cc50e5fb3a5e27a0854538d051e1
charles <charles.98006@gmail.com>
2024-01-19 18:25:36 -0800

    Revert "small singleton instance of the triggerManager"
    This reverts commit c90f0cbfa5e1e6ada68d9169ae2b5e027ebf3c00.

--- TriggerManager.cs
+++ TriggerManager.cs
@@ -1,8 +1,6 @@
-﻿using GameJam2024.RuneTree;
-using GameJam2024.RuneTree.FireRunes;
+﻿using GameJam2024.RuneTree.FireRunes;
 using GameJam2024.RuneTree.IceRunes;
 using GameJam2024.RuneTree.LightningRunes;
-using Godot;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,14 +12,28 @@ namespace GameJam2024;
 /// <summary>
 /// Class <c>TriggerManager</c> handles all of the events relating to the trigger interactable items and environments.
 /// </summary>
-public partial class TriggerManager : Node
+public sealed class TriggerManager
 {
-    public static TriggerManager Instance { get; } = new();
-
-
-    //TODO:  Need to add to the character controller when the wizard cast a spell 
-    public delegate void SpellCastingEventHandler(SpellRuneBase spell, IEnvironmentObject target);
-
-    public event SpellCastingEventHandler SpellCastingEvent;
-
+    private TriggerManager(){}
+    private void EventTriggerHandler(EventObject event)
+    {
+        int day = 4;
+        switch (action) 
+        {
+            case "BURN":
+                // code block
+                break;
+            case "BLAST":
+                // code block
+                break;
+            case "BLAST":
+                // code block
+                break;
+            case "":
+                // code block
+                break;
+            default:
+                break;
+        }
+    }
 }
