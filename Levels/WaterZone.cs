45600db7cf3ee50b43c57dac198ac04132ebd125
Matthew Brannen <brannen.matthew@gmail.com>
2024-01-23 07:24:20 +0000

    Merge pull request 19 from feature/gameover_screen into WORKING

---
+++ Levels/WaterZone.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class WaterZone : Area2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+        BodyEntered += WaterZoneBodyEntered;
+	}
+
+    private void WaterZoneBodyEntered(Node2D body)
+    {
+        GD.Print($"something collided:{body.Name}");
+        if (body.IsInGroup("player"))
+        {
+            //todo: send event to game manager that player died
+            GD.Print("Player Died");
+        };
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+	{
+	}
+}
