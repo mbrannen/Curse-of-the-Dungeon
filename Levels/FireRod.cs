a682d78f4b5f95065b0038ed1b5602a1b08d068b
charles <charles.98006@gmail.com>
2024-01-24 16:14:16 -0800

    Level1 Puzzle Working with triggers

---
+++ Levels/FireRod.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+using System.Xml.Linq;
+
+public partial class FireRod : Area2D
+{
+    public bool IsTriggered { get; set; }
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+        AreaEntered += FireRodAreaEntered;
+        IsTriggered = false;
+    }
+
+    private void FireRodAreaEntered(Area2D area)
+    {
+        GD.Print($"Spell collided:{area.Name}");
+        if (area.IsInGroup("fire") && !IsTriggered)
+        {
+            GD.Print("Fire Rod activated");
+            IsTriggered = true;
+        }
+    }
+}
---
+++ Levels/IceRod.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+using System.Xml.Linq;
+
+public partial class IceRod : Area2D
+{
+    public bool IsTriggered { get; set; }
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+        AreaEntered += IceRodAreaEntered;
+        IsTriggered = false;
+    }
+
+    private void IceRodAreaEntered(Area2D area)
+    {
+        GD.Print($"Spell collided:{area.Name}");
+        if (area.IsInGroup("ice") && !IsTriggered)
+        {
+            GD.Print("Ice Rod activated");
+            IsTriggered = true;
+        }
+    }
+}
